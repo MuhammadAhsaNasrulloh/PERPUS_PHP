ca4755cc7a8b01ba8ac00bf59f48d7aacb176e28
MuhammadAhsaNasrulloh <san.asrullah07@gmail.com>
2024-01-12 10:53:45 +0700

    update file

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,32 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard PERPUSTAKAAN SMKN 1 SUBANG</title>
-
-    <style>
-        *{
-            margin : 0;
-            padding : 0;
-        }
-        body {
-            font-family : sans-serif;
-        }
-        .card{
-            align-items : center;
-            justify-content : center;
-            width : max-content;
-            background-color : yellow;
-            padding : 15px;
-            margin : 50px;
-        }
-        .card a{
-            text-decoration: none;
-            color : black;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="card">
-        <a href="index.php">Data Buku</a>
+        <!-- As a heading -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid d-flex">
+        <a class="navbar-brand text-bold" href="#">PERPUSTAKAAN SMKN 1 SUBANG</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse me-5 d-flex justify-content-end" id="navbarNavAltMarkup" >
+        <div class="navbar-nav " >
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+        </div>
+        </div>
     </div>
+    </nav>
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+            SideBar
+            </div>
+            <div class="col">
+                <div class="card mt-5" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">Data Buku</h5>
+                <a href="index.php" class="btn btn-primary">Lihat Data</a>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mt-5" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">Data Buku</h5>
+                <a href="index.php" class="btn btn-primary">Lihat Data</a>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mt-5" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">Data Buku</h5>
+                <a href="index.php" class="btn btn-primary">Lihat Data</a>
+                </div>
+                </div>
+            </div>
+            
+        </div>
+        </div>
+    <!-- Bootstrap 5 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
--- admin/index.php
+++ admin/index.php
@@ -8,28 +8,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PERPUSTAKAAN SMKN 1 SBG</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <h1>PERPUS SMKN 1 SBG</h1>
     <!-- Create atau menambahkan data baru, Data akan di proses dan dikirim ke add.php -->
-    <form method="post" action="add.php">
-        <input type="text" name="Nama_buku" placeholder="Nama Buku" required><br>
-        <input type="text" name="Nama_penerbit" placeholder="Nama Penerbit" required><br>
-        <input type="number" name="Jumlah_buku" placeholder="Jumlah Buku" required><br>
-        <input type="date" name="Tahun_terbit" placeholder="Tahun terbit" required><br>
-        <input type="submit" name="submit" value="Tambah Data">
-    </form>
-    <!-- read atau menampilka data dari database -->
-    <table border="1">              
-        <tr>
-            <th>No.</th>
-            <th>Nama Buku</th>
-            <th>Nama Penerbit</th>
-            <th>Jumlah buku</th>
-            <th>Tahun terbit</th>
-            <th colspan="2">Aksi</th>
-        </tr>
     
+    <div class="card">
+    <div class="card mt-5">
+    <div class="card-body">
+    <div class="card-title"><h3>Formulir Data Buku</h3></div>
+    <form method="post" action="add.php">
+        <div class="mb-3">
+            <label for="" class="form-label">Nama Buku</label><br>
+            <input type="text" name="Nama_buku" placeholder="Nama Buku" required><br>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Nama Penerbit</label><br>
+            <input type="text" name="Nama_penerbit" placeholder="Nama Penerbit" required><br>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Jumlah Buku</label><br>
+            <input type="number" name="Jumlah_buku" placeholder="Jumlah Buku" required><br>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Tahun Terbit</label><br>
+            <input type="date" name="Tahun_terbit" placeholder="Tahun terbit" required><br>
+        </div>
+        <input type="submit" name="submit" value="Tambah Data" class="btn btn-primary">
+    </form>
+    </div>
+    </div>
+    </div>
+    <!-- read atau menampilka data dari database -->
+    <table class="table table-dark table-striped mt-5">
+        <thead>              
+        <tr>
+            <th scope="col">No.</th>
+            <th scope="col">Nama Buku</th>
+            <th scope="col">Nama Penerbit</th>
+            <th scope="col">Jumlah buku</th>
+            <th scope="col">Tahun terbit</th>
+            <th scope="col" colspan="2">Aksi</th>
+        </tr>
+        </thead>
     <?php
         //tampilkan semua data 
         $q = $conn->query("SELECT * FROM buku");
@@ -42,12 +64,13 @@
             <td><?= $dt['Nama_penerbit']?></td>
             <td><?= $dt['Jumlah_buku']?></td>
             <td><?= $dt['Tahun_terbit']?></td>
-            <td><a href="update.php?id=<?= $dt['Id_buku']?>">Ubah</a></td>
-            <td><a href="delete.php?id=<?= $dt['Id_buku']?>" onclick="return confirm('Anda yakin menghapus data ini?')">Hapus</a></td>
+            <td><a href="update.php?id=<?= $dt['Id_buku']?>" class="text-light">Ubah</a></td>
+            <td><a href="delete.php?id=<?= $dt['Id_buku']?>" class="text-light" onclick="return confirm('Anda yakin menghapus data ini?')">Hapus</a></td>
         </tr>
     <?php 
         endwhile;
     ?>
     </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
